9a90c2919c9e154b461e967f2097c9fa45bbbaa4
lizw <1183409807@qq.com>
2020-11-30 20:43:25 +0800

    ...

--- src/utils/message.ts
+++ src/utils/message.ts
@@ -1,4 +1,4 @@
-import { hasValue } from './utils';
+import {hasValue} from './utils';
 
 /** 订阅消息key类型 */
 type SubscribeKey = string | string[];
@@ -31,7 +31,7 @@ const messageSource: { [key: string]: any } = {};
  * @param key     订阅的消息key
  * @param handler 指定取消的监听函数
  */
-function unsubscribe<T = any>(key: SubscribeKey, handler?: Handler<T>) {
+function unsubscribe<T = any>(key: SubscribeKey, handler?: Handler<T>): void {
   const unregisterObserver = (messageKey: string) => {
     const observerArr = observerTable[messageKey];
     if (hasValue(observerArr) || observerArr.length <= 0) return;
@@ -89,7 +89,7 @@ function subscribe<T = any>(key: SubscribeKey, handler: Handler<T>): CancelSubsc
  * @param key     订阅的消息key
  * @param handler 监听函数
  */
-function subscribeOnce<T = any>(key: string, handler: Handler<T>) {
+function subscribeOnce<T = any>(key: string, handler: Handler<T>): void {
   const cancelSubscribe = subscribe(key, (data: T) => {
     try {
       handler(data, key);
@@ -105,7 +105,7 @@ function subscribeOnce<T = any>(key: string, handler: Handler<T>) {
  * @param key     消息key
  * @param message 消息值
  */
-function publish<T = any>(key: SubscribeKey, message: T) {
+function publish<T = any>(key: SubscribeKey, message: T): void {
   const value: any = message || {};
   const noticeObserver = (messageKey: string): void => {
     // 修改source的值
@@ -128,38 +128,86 @@ function publish<T = any>(key: SubscribeKey, message: T) {
   }
 }
 
-// /** 消息订阅者注册表 */
-// const observerTable: { [key: string]: Handler[] } = {};
-// /** 消息数据 */
-// const messageSource: { [key: string]: any } = {};
+class ObserveStore {
+  // 消息数据
+  private store: { [key: string]: any } = {};
+  // 消息订阅者注册表
+  private observeStoreTable: { [key: string]: Handler[] } = {};
 
+  constructor() {
+    this.init();
+  }
+
+  // 初始化
+  private init(): void {
 // 更改store值就会自动publish消息 - 实现
-const observeStore = new Proxy<any>(
-  {},
-  {
-    get(_, key: string) {
-      return messageSource[key];
-    },
-    // 代理object赋值操作,设置值的时候,触发订阅时的回调函数
-    set(obj: any, key: string, value: any): boolean {
-      // 只有值变化,才触发回调
-      if (messageSource[key] && messageSource[key] === value) return true;
-      // 消息通知订阅者
-      obj[key] = value;
-      const storeKey = key;
-      if (hasValue(storeKey)) return true;
-      const observerArr = observerTable[storeKey];
-      if (hasValue(observerArr) || observerArr.length <= 0) return true;
-      observerArr.forEach(handler => {
-        try {
-          handler(value, storeKey);
-        } catch (err) {
-          console.error("消息通知函数执行失败,messageKey=", storeKey, " | value=", value);
-        }
-      });
-      return true;
-    },
-  },
-);
+    const observeStore = new Proxy<any>(
+      {},
+      {
+        get(target: any, key: string) {
+          return target[key];
+        },
+        // 代理object赋值操作,设置值的时候,触发订阅时的回调函数
+        set(obj: any, key: string, value: any): boolean {
+          // 只有值变化,才触发回调
+          if (messageSource[key] && messageSource[key] === value) return true;
+          // 消息通知订阅者
+          obj[key] = value;
+          const storeKey = key;
+          if (hasValue(storeKey)) return true;
+          const observerArr = observerTable[storeKey];
+          if (hasValue(observerArr) || observerArr.length <= 0) return true;
+          observerArr.forEach(handler => {
+            try {
+              handler(value, storeKey);
+            } catch (err) {
+              console.error("消息通知函数执行失败,messageKey=", storeKey, " | value=", value);
+            }
+          });
+          return true;
+        },
+      },
+    );
 
-export { Handler, CancelSubscribe, subscribe, subscribeOnce, unsubscribe, publish, observeStore, }
+  }
+
+
+  /**
+   * 取消订阅
+   * @param key     订阅的消息key
+   * @param handler 指定取消的监听函数
+   */
+  public unsubscribe<T = any>(key: SubscribeKey, handler?: Handler<T>): void {
+
+  }
+
+  /**
+   * 消息订阅
+   * @param key     订阅的消息key
+   * @param handler 监听函数
+   */
+  public subscribe<T = any>(key: SubscribeKey, handler: Handler<T>): CancelSubscribe {
+
+  }
+
+  /**
+   * 消息订阅(只订阅一次)
+   * @param key     订阅的消息key
+   * @param handler 监听函数
+   */
+  public subscribeOnce<T = any>(key: string, handler: Handler<T>): void {
+
+  }
+
+  /**
+   * 发送消息(触发订阅监听函数)
+   * @param key     消息key
+   * @param message 消息值
+   */
+  public publish<T = any>(key: SubscribeKey, message: T): void {
+
+  }
+}
+
+
+export {Handler, CancelSubscribe, subscribe, subscribeOnce, unsubscribe, publish, observeStore,}
