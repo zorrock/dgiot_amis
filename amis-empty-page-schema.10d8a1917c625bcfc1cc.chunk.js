7ddf72d31175cd92bf2f026613ffffe33735e79b
h7ml <h7ml@qq.com>
2021-10-15 07:14:22 +0000

    Deploying to gh-pages from  @ e4d6ee46e36a08d5ba3d6476f9900be117779efd 🚀

--- amis-empty-page-schema.10d8a1917c625bcfc1cc.chunk.js
+++ amis-empty-page-schema.10d8a1917c625bcfc1cc.chunk.js
@@ -7,4 +7,4 @@
  *  author: h7ml(h7ml@qq.com) 
  *  time:Fri Oct 15 2021 07:10:59 GMT+0000 (Coordinated Universal Time)
  */
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{VZWQ:function(o,n,a){"use strict";a.r(n),a.d(n,"schema",(function(){return e}));var e={type:"page",body:{actionType:"dialog",dialog:{body:{}}}}}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[15],{tRAS:function(t,e,n){"use strict";n.r(e),n.d(e,"schema",(function(){return o}));var o={type:"page",title:"",toolbar:[],body:[{type:"html",html:"<h3>空白页</h3>"}]}}}]);
